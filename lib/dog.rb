c0ab074744071c7974ee2dbe464c74d1bbe02e9a
Scott Ungchusri <animoogle504@gmail.com>
2018-07-18 04:25:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  
+  
+  def name=(dog_name)
+    @this_dogs_name = dog_name 
+  end 
+  def name
+    @this_dogs_name
+  end
+end
+
+charlesBARKley = Dog.new 
+charlesBARKley.name = "Charls Barkley"
+
+charlesBARKley.name 
